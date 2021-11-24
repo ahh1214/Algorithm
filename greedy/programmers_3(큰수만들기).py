6585610dacf44d1f2eee32ac26e9925d1e2bce49
ahh1214 <ahh1214@naver.com>
2021-11-24 22:49:08 +0900

    Add Test-case

--- greedy/programmers_3(큰수만들기).py
+++ greedy/programmers_3(큰수만들기).py
@@ -7,3 +7,11 @@ def solution(number, k):
     result.append(list(map(''.join, combinations(number, num_len))))
     
     return max(result[0])
+
+a_case = solution("1924",2) #94
+b_case = solution("1231234", 3) #3234
+c_case = solution("4177252841", 4) #775841
+
+print(a_case)
+print(b_case)
+print(c_case)
