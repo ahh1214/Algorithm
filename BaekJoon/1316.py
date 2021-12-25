651997068ae56536d7ea5ba949baba517989f2e2
ahh1214 <ahh1214@naver.com>
2021-12-25 20:19:22 +0900

    코드 수정

--- BaekJoon/1316.py
+++ BaekJoon/1316.py
@@ -1,8 +1,21 @@
+# wd_len = int(input())
+
+# wd_cnt = 0
+# for i in range(wd_len):
+#     wd = input()
+#     for j in range(len(wd)-1):
+#         print(wd.find(wd[j]))
+#         print(wd.find(wd[j+1]))
+
 wd_len = int(input())
 
-wd_cnt = 0
+wd_cnt = wd_len
 for i in range(wd_len):
     wd = input()
     for j in range(len(wd)-1):
-        print(wd.find(wd[j]))
-        print(wd.find(wd[j+1]))
+        if wd[j] == wd[j+1]:
+            pass
+        elif wd[j] in wd[j+1:]:
+            wd_cnt -=1
+            break
+print(wd_cnt)
