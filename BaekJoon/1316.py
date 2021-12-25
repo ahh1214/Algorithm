e528c6f5460276076d3de2689a928b4eb002c3e2
ahh1214 <ahh1214@naver.com>
2021-12-25 23:34:31 +0900

    add 1316 (재확인)

--- BaekJoon/1316.py
+++ BaekJoon/1316.py
@@ -1,21 +1,12 @@
-# wd_len = int(input())
-
-# wd_cnt = 0
-# for i in range(wd_len):
-#     wd = input()
-#     for j in range(len(wd)-1):
-#         print(wd.find(wd[j]))
-#         print(wd.find(wd[j+1]))
-
 wd_len = int(input())
 
 wd_cnt = wd_len
 for i in range(wd_len):
     wd = input()
     for j in range(len(wd)-1):
-        if wd[j] == wd[j+1]:
+        if wd[j]==wd[j+1]: #알파벳이 연속해서 나오는 경우
             pass
-        elif wd[j] in wd[j+1:]:
+        elif wd[j] in wd[j+1:]: #연속하지않는데 뒤에 존재
             wd_cnt -=1
-            break
+        
 print(wd_cnt)
