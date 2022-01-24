f58efea38c669fcf05796e42e16211c0f4842103
ahh1214 <ahh1214@naver.com>
2022-01-24 22:09:32 +0900

    add 2869 달팽이는 올라가고 싶다 [시간초과 수정]

--- BaekJoon/2869.py
+++ BaekJoon/2869.py
@@ -1,13 +1,7 @@
 import sys
+import math
+
 A, B, V = map(int, sys.stdin.readline().split())
 
-day = 0
-while True: 
-    day += 1 #하루 경과
-    V -= A
-    if V == 0: #정상에 올라간순간 빠져나옴
-        break 
-    V += B
-    
-print(day)
-    
+day = (V-B) / (A-B)  #올라갈 수 있는 기간을 한 번에 계산    
+print(math.ceil(day)) #걸리는 시간이 소수점일 경우, 올림
